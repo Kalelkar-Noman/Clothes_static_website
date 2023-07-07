81163062e23c7587dbad83d831fcb0a2eb813781
Kalelkar-Noman <kalelkarnoman014@gmail.com>
2023-07-07 16:32:20 +0530

    updated

--- Index.js
+++ Index.js
@@ -1,12 +1,1 @@
-// Create a named function instead of using arrow functions
-function handleIntersection(entries) {
-  entries
-    .filter(entry => entry.intersectionRatio > 0)
-    .forEach(entry => entry.target.classList.add("inview"));
-}
-// Use a more specific selector to target only the elements that need to be observed
-const elements = document.getElementsByClassName("fade-up");
-// Create a new IntersectionObserver instance, passing in the handleIntersection function
-const observer = new IntersectionObserver(handleIntersection);
-// Observe each element individually
-Array.from(elements).forEach(element => observer.observe(element));
+function handleIntersection(e) { e.filter(e => e.intersectionRatio > 0).forEach(e => e.target.classList.add("inview")) } const elements = document.getElementsByClassName("fade-up"), observer = new IntersectionObserver(handleIntersection); Array.from(elements).forEach(e => observer.observe(e));
