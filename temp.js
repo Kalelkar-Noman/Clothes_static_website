61dab6e9fd9836b1f513911e0a395f1960c99b27
Kalelkar-Noman <kalelkarnoman014@gmail.com>
2023-07-07 16:19:04 +0530

    updated

---
+++ temp.js
@@ -0,0 +1,1 @@
+function handleIntersection(e) { e.filter(e => e.intersectionRatio > 0).forEach(e => e.target.classList.add("inview")) } const elements = document.getElementsByClassName("fade-up"), observer = new IntersectionObserver(handleIntersection); Array.from(elements).forEach(e => observer.observe(e));
