d17db62d4b8f9657d693a7869433001801ab352b
Kalelkar-Noman <kalelkarnoman014@gmail.com>
2023-08-16 12:15:36 +0530

    update

--- Index.js
+++ Index.js
@@ -1,3 +1,4 @@
+
 function handleIntersection(e) {
   e.filter((e) => e.intersectionRatio > 0).forEach((e) =>
     e.target.classList.add("inview")
@@ -7,7 +8,4 @@ const elements = document.getElementsByClassName("fade-up"),
   observer = new IntersectionObserver(handleIntersection);
 Array.from(elements).forEach((e) => observer.observe(e));
 
-const myEnvVar = process.env.api;
-console.log(myEnvVar)
-// const netlifyConfig = require('netlify-config');
-// const myEnvVar = netlifyConfig.env.api;
+async function does() {
